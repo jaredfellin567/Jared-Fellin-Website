e928134103d55ac6df6d8c4374aff9cf7818929a
jaredfellin567 <135291955+jaredfellin567@users.noreply.github.com>
2025-01-25 22:10:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let homeButton = document.getElementById('home');
+let cyberFellinButton = document.getElementById('webName');
+let projectsButton = document.getElementById('projects');
+let aboutMeButton = document.getElementById('aboutMe');
+
+let bodyElement = document.body;
+let dropDownMenu = document.getElementsByClassName("dropdown-content");
+let dropDownButton = document.getElementsByClassName('dropbtn');
+let drophomeButton = document.getElementById('drophome');
+let dropaboutButton = document.getElementById('dropabout');
+let dropprojectsButton = document.getElementById('dropprojects');
+let dropresumeeButton = document.getElementById('dropresumee');
+
+function goHome(){
+    window.location.href = "home.html"
+}
+
+function goProjects(){
+        window.location.href = 'projects.html'
+}
+
+function goAbout(){
+        window.location.href = 'aboutMe.html'
+}
+
+function closeMenu(){
+        dropDownMenu.style.display = "none";
+}
+
+cyberFellinButton.addEventListener('click', goHome)
+homeButton.addEventListener('click', goHome)
+drophomeButton.addEventListener('click', goHome)
+
+dropaboutButton.addEventListener('click', goAbout)
+aboutMeButton.addEventListener('click', goAbout)
+
+dropprojectsButton.addEventListener('click', goProjects)
+projectsButton.addEventListener('click', goProjects)
+
+bodyElement.addEventListener('click', closeMenu)
+
